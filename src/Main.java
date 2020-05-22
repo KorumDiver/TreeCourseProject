0521a3db37053ffdec4c7ca52beaed0a57edf635
KorumDiver <44710888+KorumDiver@users.noreply.github.com>
2020-05-22 14:01:54 +0300

    Тестовый коммит для проверки работоспособности файла gitignore

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ class Main {
         Test test = new Test();
         long[] all = test.time(50000);
 
+        //Коммит
         for (int i = 0; i < all.length; i++) {
             System.out.print(all[i] + ";");
         }
